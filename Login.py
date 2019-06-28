f56becb26b232fe8d4c6b539c863b2d9a67a8371
Umang Singh <43104796+InsideU@users.noreply.github.com>
2019-06-28 08:06:53 +0000

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,8 +1,15 @@
+import time                                                                                                                        
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-
 driver=webdriver.Chrome()
 driver.get("https://vtop.vit.ac.in/vtop")
-Submit=driver.findElement(By.cssSelector("button[type='submit']"));
-Submit.click()
+submit=driver.find_elements_by_xpath("//button[contains(text(),'Login to V-TOP')]")[0]
+submit.click()
+time.sleep(1)
+username=driver.find_element_by_css_selector("#uname")
+username.send_keys("18BCE3212")
+password=driver.find_element_by_css_selector("#passwd")
+password.send_keys("asdfklsdf")
+time.sleep(2)
+driver.find_elements_by_xpath("//button[contains(text(),'Sign in')]")[0].click()
